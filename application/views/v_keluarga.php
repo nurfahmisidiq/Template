b96f83f586907c833c7c3b7ce4edc247ad772157
nur fahmi sidiq <nur_fahmi_26rpl@student.smktelkom-mlg.sch.id>
2019-01-21 12:00:48 +0700

    first commit

---
+++ application/views/v_keluarga.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title></title>
+<style>
+    h1 {
+        text-align: center;
+        font-size: 70px;
+        font-family: chiller;
+        padding:50px 0px 20px 0px;
+        text-shadow: 2px 2px 2px black;
+
+      }
+</style>
+<marquee behavior="alternate" direction="up" scrollamount="3" scrolldelay="65" height="80" style="text-align" filter:wave></div>
+    <h1>My Sister</h1></marquee>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/adik.jpg')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Nabila Dwi Paramitha</td>
+                            </tr>
+                            <tr>
+                                <td>Pekerjaan</td> <td>: Pelajar</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 15 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Tuban</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+</head>
+
+</html>
---
+++ application/views/v_profil.php
@@ -0,0 +1,39 @@
+<<div class="block-header">
+    <h1 style="text-align: center">Biodata ku</h1>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2></h2>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/profilku.jpg')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Nur Fahmi Sidiq</td>
+                            </tr>
+                            <tr>
+                                <td>Kelas</td> <td>: XI RPL 2</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 16 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Tuban</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
